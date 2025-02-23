a384c6a6df871a3466c4b0d4ea85805fe7506275
Duncan Smith <smithduncan40@gmail.com>
2025-02-22 23:42:24 -0500

    Activity Feed

--- base/views.py
+++ base/views.py
@@ -64,6 +64,7 @@ def registerPage(request):
  
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
+
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) |
         Q(name__icontains=q) |
@@ -72,13 +73,15 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 
+               'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
     if request.method == 'POST':
         message = Message.objects.create(
